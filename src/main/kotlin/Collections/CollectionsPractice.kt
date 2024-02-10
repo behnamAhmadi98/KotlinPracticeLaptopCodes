0ac4cec1b7d614d8a69599c11197b123c1f58526
behnamAhmadi98 <141573974+behnamAhmadi98@users.noreply.github.com>
2024-02-10 21:29:03 +0100

    Lamda codes written in Laptop

--- src/main/kotlin/Collections/CollectionsPractice.kt
+++ src/main/kotlin/Collections/CollectionsPractice.kt
@@ -3,7 +3,7 @@ package Collections
 /** Kotlin has two types of collections : Mutable and Immutable
  * * 1-Immutable means that the collections are READ-ONLY
  *****List, listOf, and listOf(<T>), set->setOf, map, MapOf are all Immutable.
- *2-Mutable means that they can be read and write.  
+ *2-Mutable means that they can be read and write.
  *
  * *
  *
---
+++ src/main/kotlin/LamdaAndHigherOrderFunctions/LambdaPractice.kt
@@ -0,0 +1,9 @@
+package LamdaAndHigherOrderFunctions
+
+class LambdaPractice {
+    /**
+     * Lamda syntax Example:
+     * val lamda:(dataType1,dataType2)->{variable1:dataType1,variable2:dataType2->methodBody}
+     *
+     */
+}
