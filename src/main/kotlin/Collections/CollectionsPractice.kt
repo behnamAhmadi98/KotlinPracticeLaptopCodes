e45609f930618e0a704ef0d1cfad5dcfc4816e13
behnamAhmadi98 <141573974+behnamAhmadi98@users.noreply.github.com>
2024-02-10 21:24:48 +0100

    Kotlin practice codes written in Laptop

---
+++ src/main/kotlin/Collections/CollectionsPractice.kt
@@ -0,0 +1,13 @@
+package Collections
+
+/** Kotlin has two types of collections : Mutable and Immutable
+ * * 1-Immutable means that the collections are READ-ONLY
+ *****List, listOf, and listOf(<T>), set->setOf, map, MapOf are all Immutable.
+ *2-Mutable means that they can be read and write.  
+ *
+ * *
+ *
+ */
+fun main() {
+
+}
